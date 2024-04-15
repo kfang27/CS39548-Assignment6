c9cac3109b2d36a1404ee92788129182a979e562
Spieler727 <spieler0727@gmail.com>
2024-04-14 23:08:49 -0400

    added key to sound functionality

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ let sounds_list = [
 
 let images_list = [
     "images/crash.png",
-    "images/kick-bass.png",
+    "images/kick.png",
     "images/snare.png",
-    "images/tom-1.png",
-    "images/tom-2.png",
-    "images/tom-3.png",
-    "images/tom-4.png"
+    "images/tom1.png",
+    "images/tom2.png",
+    "images/tom3.png",
+    "images/tom4.png"
 ];
 
 //this returns a list of nodes
@@ -44,11 +44,34 @@ for (let i = 0; i < btns.length; i++){
     btns[i].addEventListener("click", function() {
         addNoise(sounds_list[i]);
     })
+    btns[i].style.backgroundImage = `url(${images_list[i]})`;
 }
-
 
 function addNoise(sound_file)
 {
     var audio = new Audio(sound_file);
     audio.play();
 }
+
+// you will see that each button has a letter(w,a,s,d,j,k,l). Add to your index.js: 
+// when you press these letters on the keyboard the sound of the corresponding drum also goes off. 
+let key_to_sound_map = {
+    'w': 'sounds/crash.mp3',
+    'a': 'sounds/kick-bass.mp3',
+    's': 'sounds/snare.mp3',
+    'd': 'sounds/tom-1.mp3',
+    'j': 'sounds/tom-2.mp3',
+    'k': 'sounds/tom-3.mp3',
+    'l': 'sounds/tom-4.mp3'
+};
+
+document.addEventListener("keydown", function(event) {
+    let key_pressed = event.key.toLowerCase();
+
+    if (key_to_sound_map.hasOwnProperty(key_pressed)){
+        let sound = key_to_sound_map[key_pressed];
+        addNoise(sound);
+    }
+});
+
+
